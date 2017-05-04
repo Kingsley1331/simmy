2548dd484aee7155a5fbbdd7ec8f57755ab35b92
Kingsley1331 <kingankomah@hotmail.co.uk>
2017-05-04 20:55:54 +0100

    Moved matrix methods to prototype

--- js/maths.js
+++ js/maths.js
@@ -1,4 +1,4 @@
-function distance(x,y){
+function distance(x, y){
 	return Math.sqrt(x*x + y*y);
 }
 
@@ -6,6 +6,7 @@ function distance(x,y){
 	 this.x = vector.x;
 	 this.y = vector.y;
 	 this.z = vector.z ? vector.z: 0;
+	 this.magnitude =  Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
  }
 
  Vector.prototype.add = function(vector2){
@@ -37,33 +38,35 @@ function Matrix(matrix){
 	this.r1 = matrix.r1;
 	this.r2 = matrix.r2;
 	this.r3 = matrix.r3;
-	this.mProd = function(matrix2){
-		var newMatrix = {};
-		var r11 = this.r1[0] * matrix2.r1[0] + this.r1[1] * matrix2.r2[0] + this.r1[2] * matrix2.r3[0];
-		var r12 = this.r1[0] * matrix2.r1[1] + this.r1[1] * matrix2.r2[1] + this.r1[2] * matrix2.r3[1];
-		var r13 = this.r1[0] * matrix2.r1[2] + this.r1[2] * matrix2.r2[2] + this.r1[2] * matrix2.r3[2];
-		newMatrix.r1 = [r11, r12, r13];
+}
 
-		var r21 = this.r2[0] * matrix2.r1[0] + this.r2[1] * matrix2.r2[0] + this.r2[2] * matrix2.r3[0];
-		var r22 = this.r2[0] * matrix2.r1[1] + this.r2[1] * matrix2.r2[1] + this.r2[2] * matrix2.r3[1];
-		var r23 = this.r2[0] * matrix2.r1[2] + this.r2[2] * matrix2.r2[2] + this.r2[2] * matrix2.r3[2];
-		newMatrix.r2 = [r21, r22, r23];
+Matrix.prototype.mMult = function(matrix2){
+	var newMatrix = {};
+	var r11 = this.r1[0] * matrix2.r1[0] + this.r1[1] * matrix2.r2[0] + this.r1[2] * matrix2.r3[0];
+	var r12 = this.r1[0] * matrix2.r1[1] + this.r1[1] * matrix2.r2[1] + this.r1[2] * matrix2.r3[1];
+	var r13 = this.r1[0] * matrix2.r1[2] + this.r1[2] * matrix2.r2[2] + this.r1[2] * matrix2.r3[2];
+	newMatrix.r1 = [r11, r12, r13];
 
-		var r31 = this.r3[0] * matrix2.r1[0] + this.r3[1] * matrix2.r2[0] + this.r3[2] * matrix2.r3[0];
-		var r32 = this.r3[0] * matrix2.r1[1] + this.r3[1] * matrix2.r2[1] + this.r3[2] * matrix2.r3[1];
-		var r33 = this.r3[0] * matrix2.r1[2] + this.r3[2] * matrix2.r2[2] + this.r3[2] * matrix2.r3[2];
-		newMatrix.r2 = [r31, r32, r33];
-		var matrix = new matrix(newMatrix);
-		return matrix;
-	};
+	var r21 = this.r2[0] * matrix2.r1[0] + this.r2[1] * matrix2.r2[0] + this.r2[2] * matrix2.r3[0];
+	var r22 = this.r2[0] * matrix2.r1[1] + this.r2[1] * matrix2.r2[1] + this.r2[2] * matrix2.r3[1];
+	var r23 = this.r2[0] * matrix2.r1[2] + this.r2[2] * matrix2.r2[2] + this.r2[2] * matrix2.r3[2];
+	newMatrix.r2 = [r21, r22, r23];
 
-	this.vProd = function(vector) {
-		var x = this.r1[0] * vector.x + this.r1[0] * vector.y + this.r1[0] * vector.z;
-		var y = this.r2[0] * vector.x + this.r2[0] * vector.y + this.r2[0] * vector.z;
-		var z = this.r3[0] * vector.x + this.r3[0] * vector.y + this.r3[0] * vector.z;
-		var product = new Vector({x: x, y: y, z: z});
-		return product;
-	}
+	var r31 = this.r3[0] * matrix2.r1[0] + this.r3[1] * matrix2.r2[0] + this.r3[2] * matrix2.r3[0];
+	var r32 = this.r3[0] * matrix2.r1[1] + this.r3[1] * matrix2.r2[1] + this.r3[2] * matrix2.r3[1];
+	var r33 = this.r3[0] * matrix2.r1[2] + this.r3[2] * matrix2.r2[2] + this.r3[2] * matrix2.r3[2];
+	newMatrix.r2 = [r31, r32, r33];
+	var matrix = new matrix(newMatrix);
+	return matrix;
+};
+
+Matrix.prototype.vMult = function(vector) {
+	vector.z = vector.z ? vector.z: 0;
+	var x = this.r1[0] * vector.x + this.r1[0] * vector.y + this.r1[0] * vector.z;
+	var y = this.r2[0] * vector.x + this.r2[0] * vector.y + this.r2[0] * vector.z;
+	var z = this.r3[0] * vector.x + this.r3[0] * vector.y + this.r3[0] * vector.z;
+	var product = new Vector({x: x, y: y, z: z});
+	return product;
 }
 
 // var v1 = new Vector({x:1, y:3, z:0});
@@ -83,3 +86,6 @@ console.log('v3', v3);
 console.log('v4', v4);
 console.log('v5', v5);
 console.log('v6', v6);
+
+var v10 = new Vector({x:3, y:4, z:8});
+console.log('v10.magnitude', v10.magnitude);
