e102cb9853268ca7d4265f099ed1aafe6303a8bd
Kingsley1331 <kingankomah@hotmail.co.uk>
2019-01-26 17:35:49 +0000

    server - Now serving the app from express server

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const path = require('path');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
 
 // cookieSession takes the cookie data(mongodb user id) and attatches it to the req.session property
 app.use(
@@ -24,5 +32,12 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/user')(app);
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+app.listen(PORT, function () {
+  console.log('Express server listening on port ' + PORT);
+});
