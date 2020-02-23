a9f8c16330ef371c2d6811af49d94a2effa0cc1c
kingsley1331 <kingsley.ankomah@gmail.com>
2020-02-23 02:50:55 +0000

    Dynamically generate multiple conditions

--- client/src/ui/components/events/rule.js
+++ client/src/ui/components/events/rule.js
@@ -53,11 +53,27 @@ const Rule = ({
       e.target.value;
     resetRules();
   };
+
+  const updateLogicalOperator = (e, operatorIndex) => {
+    rules[eventType][selectedShapeId][index].logicalOperators[operatorIndex] =
+      e.target.value;
+    resetRules();
+  };
+
   const addAction = () => {
     rules[eventType][selectedShapeId][index].actions.push({
       comparison: "",
       operatorValue: ""
     });
+    resetRules();
+  };
+  const addCondition = () => {
+    rules[eventType][selectedShapeId][index].conditions.push({
+      propertyName: "",
+      actionPropertyName: "",
+      newValue: ""
+    });
+    rules[eventType][selectedShapeId][index].logicalOperators.push("OR");
     resetRules();
   };
 
@@ -72,80 +88,59 @@ const Rule = ({
       <div>
         <h3>Conditions:</h3>
         {/******************************************************************************** CONDITION 1 */}
-        Property name:&nbsp; &nbsp;
-        <select
-          onChange={updateRules("propertyName", "conditions", 0)}
-          defaultValue={rule.conditions[0].propertyName}
-          // defaultValue={rule.propertyName}
-          className="propertyName"
-        >
-          {propertiesArray.map(property => (
-            <option key={property} value={property}>
-              {property}
-            </option>
-          ))}
-        </select>
-        &nbsp; &nbsp; operator &nbsp; &nbsp;
-        <select
-          defaultValue={rule.conditions[0].operatorValue}
-          // defaultValue={rule.operatorValue}
-          onChange={updateRules("operatorValue", "conditions", 0)}
-        >
-          {operatorsArray.map(property => (
-            <option key={property[1]} value={property[0]}>
-              {property[1]}
-            </option>
-          ))}
-        </select>
-        &nbsp; &nbsp; comparison value:&nbsp; &nbsp;
-        <input
-          onBlur={updateRules("comparison", "conditions", 0)}
-          defaultValue={rule.conditions[0].comparison}
-          // defaultValue={rule.comparison}
-        />
-        <br></br>
-        <br></br>
-        <select defaultValue={rule.logicalOperators[0]}>
-          <option value="OR">OR</option>
-          <option value="AND">AND</option>
-          <option value="NOT">NOT</option>
-        </select>
-        <br></br>
-        <br></br>
-        {/******************************************************************************** CONDITION 2 */}
-        Property name:&nbsp; &nbsp;
-        <select
-          onChange={updateRules("propertyName", "conditions", 1)}
-          defaultValue={rule.conditions[1].propertyName}
-          // defaultValue={rule.propertyName}
-          className="propertyName"
-        >
-          {propertiesArray.map(property => (
-            <option key={property} value={property}>
-              {property}
-            </option>
-          ))}
-        </select>
-        &nbsp; &nbsp; operator &nbsp; &nbsp;
-        <select
-          defaultValue={rule.conditions[1].operatorValue}
-          // defaultValue={rule.operatorValue}
-          onChange={updateRules("operatorValue", "conditions", 1)}
-        >
-          {operatorsArray.map(property => (
-            <option key={property[1]} value={property[0]}>
-              {property[1]}
-            </option>
-          ))}
-        </select>
-        &nbsp; &nbsp; comparison value:&nbsp; &nbsp;
-        <input
-          onBlur={updateRules("comparison", "conditions", 1)}
-          defaultValue={rule.conditions[1].comparison}
-          // defaultValue={rule.comparison}
-        />
+        {conditions.map((condition, index) => (
+          <div>
+            Property name:&nbsp; &nbsp;
+            <select
+              onChange={updateRules("propertyName", "conditions", index)}
+              defaultValue={condition.propertyName}
+              // defaultValue={rule.propertyName}
+              className="propertyName"
+            >
+              {propertiesArray.map(property => (
+                <option key={property} value={property}>
+                  {property}
+                </option>
+              ))}
+            </select>
+            &nbsp; &nbsp; operator &nbsp; &nbsp;
+            <select
+              defaultValue={condition.operatorValue}
+              // defaultValue={rule.operatorValue}
+              onChange={updateRules("operatorValue", "conditions", index)}
+            >
+              {operatorsArray.map(property => (
+                <option key={property[1]} value={property[0]}>
+                  {property[1]}
+                </option>
+              ))}
+            </select>
+            &nbsp; &nbsp; comparison value:&nbsp; &nbsp;
+            <input
+              onBlur={updateRules("comparison", "conditions", index)}
+              defaultValue={condition.comparison}
+              // defaultValue={rule.comparison}
+            />
+            <br></br>
+            <br></br>
+            {index + 1 < numOfConditions && (
+              <div>
+                <select
+                  defaultValue={rule.logicalOperators[index]}
+                  onChange={e => updateLogicalOperator(e, index)}
+                >
+                  <option value="OR">OR</option>
+                  <option value="AND">AND</option>
+                  <option value="NOT">NOT</option>
+                </select>
+                <br></br>
+                <br></br>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-
+      <button onClick={addCondition}>Add condition</button>
       <div>
         <h3>Actions:</h3>
         {/******************************************************************************** ACTION 1 */}
